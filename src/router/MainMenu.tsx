43552b42aae604def05016798b26087f1fd4bcc8
Maxim Velesyuk <max.velesyuk@gmail.com>
2024-10-26 23:25:56 +0200

    Update main menu layout

--- src/router/MainMenu.tsx
+++ src/router/MainMenu.tsx
@@ -1,57 +1,58 @@
-import { VolumeOff, VolumeUp } from "@mui/icons-material";
-import { Box, Button, Slider, Typography } from "@mui/material";
-import ReleaseNotes from "components/ReleaseNotes";
-import { MusicContext } from "contexts/MusicContext";
-import { useContext } from "react";
-import { useNavigate } from "react-router";
+import { VolumeOff, VolumeUp } from '@mui/icons-material'
+import { Box, Button, Slider, Typography } from '@mui/material'
+import ReleaseNotes from 'components/ReleaseNotes'
+import { MusicContext } from 'contexts/MusicContext'
+import { useContext } from 'react'
+import { useNavigate } from 'react-router'
 
 type Props = {
-  title: string;
-};
+  title: string
+}
 
 export default function MainMenu({ title }: Props) {
-  const navigate = useNavigate();
-  const { isMuted, setIsMuted, volume, setVolume } = useContext(MusicContext);
+  const navigate = useNavigate()
+  const { isMuted, setIsMuted, volume, setVolume } = useContext(MusicContext)
   return (
     <Box>
       <Box
         sx={{
-          textAlign: "left",
-          float: "left",
-          position: "absolute",
+          textAlign: 'left',
+          float: 'left',
+          position: 'absolute',
           paddingLeft: 6,
           paddingTop: 3,
-          display: "flex",
-          width: "100%",
+          display: 'flex',
+          justifyContent: 'space-between',
+          width: '100%',
         }}
       >
-        <Typography variant="body2" style={{ cursor: "pointer" }} width="20%">
-          <Button onClick={() => navigate("/turnir")}>Турнир</Button>
-          <Button onClick={() => navigate("/voting")}>Голосование</Button>
-        </Typography>
-        <Box display="flex" alignItems="center" width="20%" paddingLeft={1}>
-          <Button variant="outlined" onClick={() => setIsMuted(!isMuted)} sx={{ marginRight: 2 }}>
-            {isMuted ? <VolumeOff /> : <VolumeUp />}
+        <Box display="flex">
+          <Button onClick={() => navigate('/turnir')} variant="text">
+            Турнир
           </Button>
-          <Slider
-            aria-label="Volume"
-            value={volume}
-            min={0}
-            max={1}
-            step={0.01}
-            onChange={(_evt, value) => {
-              setVolume(value as number);
-            }}
-            sx={{ marginRight: 2 }}
-          />
+          <Button onClick={() => navigate('/voting')}>Голосование</Button>
         </Box>
-        <Box
-          sx={{
-            textAlign: "right",
-            // position: "absolute",
-            width: "100%",
-          }}
-        >
+        <Box display="flex">
+          <Box display="flex" alignItems="flex-end" width="250px">
+            <Button
+              variant="outlined"
+              onClick={() => setIsMuted(!isMuted)}
+              sx={{ marginRight: 2 }}
+            >
+              {isMuted ? <VolumeOff /> : <VolumeUp />}
+            </Button>
+            <Slider
+              aria-label="Volume"
+              value={volume}
+              min={0}
+              max={1}
+              step={0.01}
+              onChange={(_evt, value) => {
+                setVolume(value as number)
+              }}
+              sx={{ marginRight: 2 }}
+            />
+          </Box>
           <ReleaseNotes />
         </Box>
       </Box>
@@ -60,14 +61,14 @@ export default function MainMenu({ title }: Props) {
         justifyContent="center"
         alignItems="center"
         style={{
-          fontWeight: "bold",
-          fontSize: "2em",
+          fontWeight: 'bold',
+          fontSize: '2em',
           margin: 20,
-          textAlign: "center",
+          textAlign: 'center',
         }}
       >
         <Box>{title}</Box>
       </Box>
     </Box>
-  );
+  )
 }
