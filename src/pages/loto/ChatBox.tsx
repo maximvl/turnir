0245845d998adc74e15ba7559b1c141068b90747
Maxim Velesyuk <max.velesyuk@gmail.com>
2025-08-03 00:13:31 +0200

    Scroll chatbox to bottom on new message
    The chatbox now scrolls to the bottom when a new message is added. This ensures that the latest
    messages are always visible to the user.

--- src/pages/loto/ChatBox.tsx
+++ src/pages/loto/ChatBox.tsx
@@ -1,15 +1,25 @@
 import { Box } from '@mui/material'
 import { ChatMessage } from '../turnir/types'
+import { useEffect, useRef } from 'react'
 
 type Props = {
   messages: ChatMessage[]
 }
 
 export default function ChatBox({ messages }: Props) {
+  const containerRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (containerRef.current) {
+      containerRef.current.scrollTop = containerRef.current.scrollHeight
+    }
+  }, [messages.length])
+
   return (
     <Box>
       {/* <Box>Сообщения {username}</Box> */}
       <Box
+        ref={containerRef}
         style={{ backgroundColor: '#222222' }}
         height={'200px'}
         // width={'inherit'}
